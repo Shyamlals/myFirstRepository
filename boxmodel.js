fa9e0ed390f2606dc849c73c6a4b6d5b2d968dc4
zapzz56 <106573107+zapzz56@users.noreply.github.com>
2022-05-31 09:50:37 +0530

    Add files via upload

---
+++ boxmodel.js
@@ -0,0 +1,10 @@
+let empName = "Philip Jackson",
+empAge = 26,
+isConfirmed = 0;
+bonus=undefined,
+designation=systemsEngineer,
+promotions=null;
+
+document.write("Primitive and non primitive data type " );
+document.write("Employee details");
+document.write("Employee Name:"+empName);
